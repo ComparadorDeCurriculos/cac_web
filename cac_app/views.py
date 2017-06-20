13bed5624db93beb8682ade9fe2fa1ddab6f2ab6
Bruno Henrique Rasteiro <brunorasteiro96@gmail.com>
2017-06-20 20:45:33 -0300

    Generating bar and radar charts

--- cac_app/views.py
+++ cac_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, render_to_response, redirect
 from .forms import CompForm
-from .src.plotting import *
+
 
 def compare(request):
 	if request.method == "POST":
@@ -16,35 +16,60 @@ def compare(request):
 
 def result(request, i1, c1, i2, c2):
 	import csv
+	import os.path
 
-	path1 = 'cac_app/cursos/' + i1 + '_' + c1 + '.txt'
-	path2 = 'cac_app/cursos/' + i2 + '_' + c2 + '.txt'
-	csv2bars = []
+	csv2chart = []
+	
+	i1 = i1.upper()
+	i2 = i2.upper()
+	c1 = c1.upper()
+	c2 = c2.upper()
 
-	with open('cac_app/static/data.csv', newline='') as csvfile:
+	path = 'cac_app/static/comparison/credAulaNuc-'+i1+'_'+c1+'-'+i2+'_'+c2+'.csv'
+	if (not os.path.isfile(path)):
+		path = 'cac_app/static/comparison/credAulaNuc-'+i2+'_'+c2+'-'+i1+'_'+c1+'.csv'		
+
+	with open(path, newline='') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		for row in spamreader:
-			csv2bars.append(row)
+			csv2chart.append(row)
 	
+	labels = []
+	
+	for l in csv2chart[2]:
+		s = l.split(' ')
+		
+		# make label for each core
+		label = []
+		if (len(s) == 3):
+			label.append(s[0]+' '+s[1])
+			label.append(s[2])
+		elif (len(s) == 4):
+			label.append(s[0]+' '+s[1])
+			label.append(s[2]+' '+s[3])
+		else:
+			label.append(l)
+
+		# inserting label
+		labels.append(label)
+
+
 	chartDic = {
-		"2bars": {
-			"labels": str(csv2bars[0]),
-			"data1":  str(list(map(int, csv2bars[1]))),
-			"data2":  str(list(map(int, csv2bars[2]))),
-	  },
-		"grafico2": {
-			"labels": str(csv2bars[0]),
-			"data1":  str(list(map(int, csv2bars[1]))),
-			"data2":  str(list(map(int, csv2bars[2]))),
+		"credAulaNuc": {
+			"title"			: csv2chart[0][0],
+			"university1"	: csv2chart[1][0], 
+			"course1"		: csv2chart[1][1],
+			"university2"	: csv2chart[1][2], 
+			"course2"		: csv2chart[1][3],
+			'l1'			: str(labels[0]),
+			'l2'			: str(labels[1]),
+			'l3'			: str(labels[2]),
+			'l4'			: str(labels[3]),
+			'l5'			: str(labels[4]),
+			'l6'			: str(labels[5]),
+			"data1"			: str(list(map(int, csv2chart[3]))),
+			"data2"			: str(list(map(int, csv2chart[4]))),
 	  }
 	}
 
-	print("=======================")
-	print(chartDic)
-	print("=======================")
-
-	#'cac_app/cursos/icmc/bcc/icmc/bsi_cac_app.txt'
-
-	#plt.plotTwoBar(ufrgs, icmc_bcc);
-
-	return render(request, 'cac_app/result.html', {'2bars': chartDic["2bars"]})
+	return render(request, 'cac_app/result.html', {'credAulaNuc': chartDic["credAulaNuc"]})
